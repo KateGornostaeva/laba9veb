6028fe08185d78176495ef26079ed164d862e5ae
Gornostaeva <kate.gornostaeva@inbox.ru>
2022-12-16 10:56:44 +0300

    laba9

---
+++ main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Горностаева Е. А. 211-362 №9</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">  <!--подключение стилей css -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"> <!--подключение шрифтов -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="wrapper">
+            <header class="header">
+                <div class="header">
+                    <div class="inline"><img src="polytech_logo.png"></div>
+                    <div class="inline title">Горностаева Е. А. 211-362 №9</div>
+                </div>
+            </header>
+
+            <main>
+                <?php
+                    include "func.php";
+                ?> 
+
+            </main>
+
+            <footer>
+                <div class="footer">
+                    <span class="left">
+                        <span>Все права защищены &copy; by Gornostaeva</span><br>
+                        <span>Тип верстки: <?=$type?></span>
+                    </span>
+                    <span class="right">
+                        <span>+7(962)443-51-66</span><br>
+                        <span>kate.gornostaeva@inbox.ru</span>
+                    </span>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
